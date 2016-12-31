f9514e74dad2fe9d68977eb8f3c1dc5820ee2054
itzjustricky <chang.ricky08@gmail.com>
2016-12-30 23:05:15 -0500

    changed setup file to add numpy include directory for c-code files

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(
-        "illumine/woodland/_retrieve_leaf_paths.pyx"
-    )
+        "illumine/woodland/_retrieve_leaf_paths.pyx"),
+    include_dirs=[numpy.get_include()]
 )
