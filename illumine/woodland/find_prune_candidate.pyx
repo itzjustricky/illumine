9463d7768e5839def9488d15a5ff048507acc272
itzjustricky <chang.ricky08@gmail.com>
2017-02-22 18:15:55 -0500

    succesfully ported over _find_prune_candidates to release GIL and use prange for parallelism; TODO investigate parallel directive

--- illumine/woodland/find_prune_candidate.pyx
+++ illumine/woodland/find_prune_candidate.pyx
@@ -2,11 +2,19 @@
     Cython module for the retrieve_leaf_path
     function used to build lucid-tree objects
 
+    TODO:
+        allow for pruning over a random subsample
+        of prune candidates
+
 """
 
 import logging
 
 cimport cython
+from cython.parallel import prange
+from cpython cimport array
+
+import numpy as np
 cimport numpy as cnp
 from numpy.math cimport INFINITY
 
@@ -14,7 +22,7 @@ from ..core cimport metrics
 
 
 # define a function type
-ctypedef double (*metric_f)(double[:] y_true, double[:] y_pred)
+ctypedef double (*metric_f)(double[:] y_true, double[:] y_pred) nogil
 
 
 def find_prune_candidates(cnp.ndarray[double, ndim=1] y_true,
@@ -63,39 +71,52 @@ def find_prune_candidates(cnp.ndarray[double, ndim=1] y_true,
         n_prunes)
 
 
-cdef _find_prune_candidates(cnp.ndarray[double, ndim=1] y_true,
-                            cnp.ndarray[double, ndim=1] y_pred,
-                            cnp.ndarray[double, ndim=2] pred_matrix,
-                            metric_f score_function,
-                            int n_prunes):
-    cdef list indices, prune_candidates
-    indices = list(range(pred_matrix.shape[1]))
-    prune_candidates = []
+cdef list _find_prune_candidates(double[:] y_true, double[:] y_pred,
+                                 double[:, :] pred_matrix,
+                                 metric_f score_function,
+                                 int n_prunes):
+    """ C-declared function to allow for the release
+        of the gil for parallel computation
+    """
+    cdef int i, j, n_indices
+    cdef int prune_ind                                  # index of candidate to be pruned
+    cdef double global_best_score, local_best_score     # use to keep track of scores
 
-    cdef int prune_ind, worst_ind
-    cdef double local_best_score, global_best_score, curr_score
-    cdef cnp.ndarray[double, ndim=1] y_pred_tmp
+    cdef int vect_size = y_true.shape[0]
+    cdef double[:] y_pred_tmp                           # tmp storage of predictions
+    cdef double[:] scores                               # store scores from pruning of candidates
 
+    # keep a cython_view and python list object
+    # of indices of prune candidates
+    cdef int[:] cv_indices
+    cdef list py_indices = list(range(pred_matrix.shape[1]))
+    cdef list prune_candidates = []
     global_best_score = score_function(y_true, y_pred)
+
+    y_pred_tmp = np.empty(vect_size, dtype=float)
     for prune_ind in range(n_prunes):
 
-        worst_ind, local_best_score = 0, -INFINITY
-        # for ind in xrange(pred_matrix.shape[1]):
-        for ind in indices:
-            # if ind in prune_candidates:
-            #     continue
+        cv_indices = array.array('i', py_indices)
+        n_indices = cv_indices.shape[0]
+        scores = np.zeros(n_indices)
 
-            y_pred_tmp = y_pred - pred_matrix[:, ind]
-            curr_score = score_function(
-                y_true=y_true,
-                y_pred=y_pred_tmp)
+        with nogil:
+            for i in prange(n_indices):
+                for j in prange(vect_size):
+                    y_pred_tmp[j] = y_pred[j] - pred_matrix[j, cv_indices[i]]
 
-            if curr_score > local_best_score:
-                logging.getLogger(__name__).debug(
-                    "For prune {} the best score was updated to {}"
-                    .format(prune_ind, curr_score))
-                worst_ind = ind
-                local_best_score = curr_score
+                scores[i] = score_function(
+                    y_true=y_true,
+                    y_pred=y_pred_tmp)
+
+            # now search for the index of the candidate
+            # whose prune produces the best score
+            prune_ind, local_best_score = 0, -INFINITY
+            for i in range(n_indices):
+                if scores[i] > local_best_score:
+                    prune_ind = cv_indices[i]
+                    local_best_score = scores[i]
+        # end of nogil
 
         if global_best_score > local_best_score:
             return prune_candidates
@@ -104,10 +125,12 @@ cdef _find_prune_candidates(cnp.ndarray[double, ndim=1] y_true,
                 "There are now {} prune candidate(s)"
                 .format(len(prune_candidates)))
 
-            y_pred = y_pred - pred_matrix[:, worst_ind]
-            indices.remove(worst_ind)
+            with nogil:
+                for i in prange(vect_size):
+                    y_pred[i] = y_pred[i] - pred_matrix[i, prune_ind]
+            py_indices.remove(prune_ind)
 
             global_best_score = local_best_score
-            prune_candidates.append(worst_ind)
+            prune_candidates.append(prune_ind)
 
     return prune_candidates
